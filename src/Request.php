f9b1fac862299197155763c6e8089de5076b88b8
Christopher Leggett <chris@leggett.dev>
2020-01-30 21:53:57 -0500

    Adds documentation comments
    Signed-off-by: Christopher Leggett <chris@leggett.dev>

--- src/Request.php
+++ src/Request.php
@@ -3,12 +3,25 @@
 namespace leggettc18\SimpleRouter;
 
 class Request {
+
+    /**
+     * Returns the currently requested URI
+     * 
+     * @static
+     * @return string
+     */
     public static function uri() {
 
         return trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
     }
 
+    /**
+     * Returns the currently requested method
+     * 
+     * @static
+     * @return string 
+     */
     public static function method() {
         return $_SERVER['REQUEST_METHOD'];
     }
--- src/Router.php
+++ src/Router.php
@@ -9,6 +9,14 @@ class Router {
         'POST' => []
     ];
 
+    /**
+     * Sets up routes by loading from a file containing routes.
+     * 
+     * Routes are declared with $route->get(uri, controller@action),
+     * or $route->post(uri, controller@action).
+     * 
+     * @param string php file containing routes
+     */
     public static function load($file) {
         $router = new static;
 
@@ -17,18 +25,37 @@ class Router {
         return $router;
     }
 
+    /**
+     * Sets up a get route.
+     * 
+     * @param $uri
+     * @param $controller 
+     */
     public function get($uri, $controller) {
 
         $this->routes['GET'][$uri] = $controller;
 
     }
 
+    /**
+     * Sets up a post route.
+     * 
+     * @param string
+     * @param string
+     */
     public function post($uri, $controller) {
 
         $this->routes['POST'][$uri] = $controller;
 
     }
 
+    /**
+     * Directs requests to the appropriate controller action.
+     * 
+     * @param string
+     * @param string
+     * @return file result of call action.
+     */
     public function direct($uri, $method) {
 
         if(array_key_exists($uri, $this->routes[$method])) {
@@ -39,6 +66,13 @@ class Router {
 
     }
 
+    /**
+     * Calls a controller action
+     * 
+     * @param string
+     * @param string
+     * @return file result of controller actionn called.
+     */
     protected function callAction($controller, $action) {
         $controller = new $controller;
 
