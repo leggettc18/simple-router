2c9b951788f9816a23d84f015e70ab1e38992728
Christopher Leggett <chris@leggett.dev>
2022-05-02 03:05:45 +0000

    Install PHPUnit

---
+++ tests/RouterTest.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class RouterTest extends TestCase {
+    public function testSample(): void {
+        $this->assertTrue(true);
+    }
+}
+?>
